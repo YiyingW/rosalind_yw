8292124ca824e678c98064f3df7efdc5281d43d8
Yiying Wang <YiyingW@users.noreply.github.com>
2015-06-17 19:12:29 -0400

    Update independent alleles.py

--- independent alleles.py
+++ independent alleles.py
@@ -1,10 +1,15 @@
-from scipy.misc import comb as choose
+from scipy.misc import comb 
  
 k = 7
 N = 31
  
 prob = 0.0
 for i in range(N,2**k+1):
-    prob += choose( 2**k, i) * (0.25**i) * (0.75**(2**k-i))
+    prob += comb(2**k, i) * (0.25**i) * (0.75**(2**k-i))
  
 print (prob)
+
+
+# the main points in this problem:
+# 1. knowing the usage of scipy.misc.comb
+# 2. the probability of getting Aa when mating with Aa is 0.25 no matter the other partner is AA, Aa, or aa. 
